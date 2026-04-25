fcd58ff5eff912e19b600032ee76f027639166dc
pritanshi-gupta <guptapritanshi@gmail.com>
2026-04-25 16:21:47 +0530

    Spring Boot project with entity and repository

---
+++ src/main/java/com/crud_operation_backend/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package com.crud_operation_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/crud_operation_backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.crud_operation_backend.repository;
+
+import com.crud_operation_backend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+}
